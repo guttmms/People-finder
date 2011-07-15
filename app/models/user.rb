cf457d2bd898a815b6917059652ed1e12207ceb2
Mark Guttman <guttmms@lakeforest.edu>
2011-07-15 13:04:10 -0500

    new commit

---
+++ app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ActiveRecord::Base
+  has_one :profile, :dependent => :destroy
+  has_many :friendships
+  has_many :friends, :through => :friendships
+  
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+  # new columns need to be added here to be writable through mass assignment
+  attr_accessible :username, :email, :password, :password_confirmation
+
+  attr_accessor :password
+  before_save :prepare_password
+  after_create :create_profile
+
+  validates_presence_of :username
+  validates_uniqueness_of :username, :email, :allow_blank => true
+  validates_format_of :username, :with => /^[-\w\._@]+$/i, :allow_blank => true, :message => "should only contain letters, numbers, or .-_@"
+  validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
+  validates_presence_of :password, :on => :create
+  validates_confirmation_of :password
+  validates_length_of :password, :minimum => 4, :allow_blank => true
+
+  def create_profile
+   @profile =  Profile.new(:user_id => self.id)
+   @profile.save
+  end
+  # login can be either username or email address
+  def self.authenticate(login, pass)
+    user = find_by_username(login) || find_by_email(login)
+    return user if user && user.password_hash == user.encrypt_password(pass)
+  end
+
+  def encrypt_password(pass)
+    BCrypt::Engine.hash_secret(pass, password_salt)
+  end
+
+  private
+
+  def prepare_password
+    unless password.blank?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = encrypt_password(password)
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SearchApp::Application.config.secret_token = 'd4c1b44cd19337c3f15af6e8d5924a8054ae2371139925ebe2b28d7c2461d0aa18d31deedf0a2643badf0b4cc2620430b075e15fc9a0ef4421a790c468a211b3'
